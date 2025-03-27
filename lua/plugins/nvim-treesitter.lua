243090ec2e8faad751a868ac9b10a49ab51a9cb6
MarceIine <ac405040500@gmail.com>
2025-03-27 19:07:01 -0400

    Update

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -1,6 +1,6 @@
 return {
   "nvim-treesitter/nvim-treesitter",
   opts = {
-    ensure_installed = { "hyprlang" },
+    ensure_installed = { "hyprlang", "git_config", "gitcommit", "git_rebase", "gitignore", "gitattributes" },
   },
 }
